7075dcc0d7517c6f14aebed937f9f26e2b368998
Tolga Topkaç <tolgatopkac09@gmail.com>
2022-11-13 05:02:21 +0300

    update index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { extendTheme } from "@chakra-ui/react";
+import WalletContextProvider from "../components/WalletContextProvider";
 
 const colors = {
   background: "#1F1F1F",
@@ -14,7 +15,9 @@ const theme = extendTheme({ colors });
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <WalletContextProvider>
+        <Component {...pageProps} />
+      </WalletContextProvider>
     </ChakraProvider>
   );
 }
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { Box, Center, Spacer, Stack } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+import Disconnected from "../components/Disconnected";
+import { useWallet } from "@solana/wallet-adapter-react";
+import Connected from "../components/Connected";
+
+const Home: NextPage = () => {
+  const { connected } = useWallet();
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Buildoors</title>
+        <meta name="The NFT Collection for Buildoors" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Box
+        w="full"
+        h="calc(100vh)"
+        bgImage={connected ? "" : "url(/home-background.svg)"}
+        bgColor={connected ? "#1F1F1F" : ""}
+        backgroundPosition="center"
+      >
+        <Stack w="full" h="calc(100vh)" justify="center">
+          <Navbar />
+
+          <Spacer />
+          <Center>{connected ? <Connected /> : <Disconnected />}</Center>
+          <Spacer />
+        </Stack>
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
